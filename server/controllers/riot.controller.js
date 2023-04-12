25ed3d6fce064645a9d21d77282eeea1ff1eae01
Aladin-Saleh <aladin8857@gmail.com>
2023-04-12 09:27:32 +0200

    routes

---
+++ server/controllers/riot.controller.js
@@ -0,0 +1,1 @@
+// Controller for Riot API (server/controllers/riot.controller.js)
---
+++ server/routes/riot.routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const riotController = require('../controllers/riot.controller');
+
+
+
+
+
+
+module.exports = router;
