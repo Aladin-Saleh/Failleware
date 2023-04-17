c715f178be069ec8f7954245c724060fdd30a4c5
Argail-GISCLARD <argail.gisclard@outlook.fr>
2023-04-17 18:11:28 +0200

    Initialisation

--- server/controllers/riot.controller.js
+++ server/controllers/riot.controller.js
@@ -1,6 +1,12 @@
 // Controller for Riot API (server/controllers/riot.controller.js)
 const axios = require('axios');
 const ngsi  = require('ngsi-parser')
+const LolApi = require('twisted').LolApi;
+const api = new LolApi(
+    {
+        key: process.env.RIOT_API_KEY,
+    }
+);
 
 
 module.exports.getSummoner = async (req, res) => 
@@ -23,20 +29,12 @@ module.exports.getSummoner = async (req, res) =>
 
     try
     {
-        const apiSummoner = await axios
-        ({
-            method:     'GET',
-            url:        url,
-            headers: 
-            {
-                'X-Riot-Token': process.env.RIOT_API_KEY
-            }
-        })
-        .catch(error => {
-            console.log("error", error.response);
-            message = error.response.data.status.message;
-            code    = error.response.data.status.status_code;
-        })
+
+
+        const apiSummoner = await api.Summoner.getByName(summonerName, region);
+        
+        
+
 
         // Parcourir les données pour les mettre au format NGSI
         const summoner          = apiSummoner.data;
@@ -91,6 +89,91 @@ module.exports.getSummoner = async (req, res) =>
         res.status(code).json(
             {
                 message:    message || 'Une erreur est survenue lors de la récupération des informations du summoner',
+                error:      error
+            }
+        )
+    }
+
+}
+
+
+module.exports.getMasteries = async (req, res) => 
+{
+    const encryptedSummonerId  = req.params.encryptedSummonerId;
+    const region        = req.query.region || 'euw1'; // Par défaut, on prend l'EUW1
+    var message         = '';
+    var code            = 500;
+
+    if (!encryptedSummonerId)
+    {
+        res.status(400).json(
+            {
+                message: 'Le nom du summoner est obligatoire'
+            }
+        );
+    }
+
+    const url = `https://${region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${encodeURIComponent(encryptedSummonerId)}`;
+
+    try
+    {
+
+
+        const apiMasteries = await api.Summoner.masteryBySummoner(encryptedSummonerId, region);      
+
+
+        // Parcourir les données pour les mettre au format NGSI
+        const masteries          = apiMasteries.data;
+        const ngsiObjectJSON    = 
+        {
+            "actionType": "APPEND",
+            "entities": []
+        } 
+
+        const entities      = {};
+
+        entities["id"]      = encryptedSummonerId;
+        entities["type"]    = "Masteries"; 
+        for (let i = 0; i<masteries.length; i++)
+        {
+            for (const key in masteries[i])
+            {
+                if (key !== "summonerId")
+                {
+                    entities[key] = ngsi.parseValue(masteries[key]);
+                }                    
+            }
+                
+        }
+
+        ngsiObjectJSON.entities.push(entities);
+        console.log("ngsiObjectJSON", ngsiObjectJSON);
+
+        // const fiwareSummoner = await axios({
+        //     method: 'POST',
+        //     url: `${process.env.FIWARE_URL}/v2/op/update`,
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     data: ngsiObjectJSON
+        // })
+        // .catch(error => {
+        //     console.log("error", error.response);
+        //     message = "Une erreur est survenue lors de la mise à jour des informations des masteries dans le contexte FIWARE"
+        //     code    = 400;            
+        // })
+
+        // res.status(200).json({
+        //     message:    'Récupération des informations des masteries',
+        //     summoner:   apiSummoner.data,
+        //     data:       ngsiObjectJSON
+        // });
+    }
+    catch (error)
+    {
+        res.status(code).json(
+            {
+                message:    message || 'Une erreur est survenue lors de la récupération des informations des masteries',
                 error:      error
             }
         )
--- server/routes/riot.routes.js
+++ server/routes/riot.routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const riotController = require('../controllers/riot.controller');
 
-
 // Récupération des informations d'un summoner par son nom
 // GET /api/riot/summoner/:summonerName?region=euw1 (region is optional)
 router.get('/summoner/:summonerName', riotController.getSummoner);
 
-
+// Récupération des informations des masteries d'unn summoner par son id
+// GET /api/riot/masteries/:encryptedSummonerId?region=euw1 (region is optional)
+router.get('/masteries/:encryptedSummonerId', riotController.getMasteries);
 
 module.exports = router;
