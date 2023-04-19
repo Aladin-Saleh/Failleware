3212b4f00205a82686140ef6a560a95969846229
Argail-GISCLARD <argail.gisclard@outlook.fr>
2023-04-20 00:09:42 +0200

    Merged main into Argail

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
+import axios from "axios";
+
+import Summoner from '../summoner/Summoner';
+import SearchBar from '../searchbar/SearchBar';
 
 const NavBar = () => {
+  const [summonerList, setSummonerList] = useState([]);
+  
+  const onSearchSubmit = async term => {
+    const res = await axios({
+      method: 'get',
+      url: `http://localhost:5000/api/fiware/summoner/${term}`
+    })
+    .then((res) => {
+        console.log(res);
+        setSummonerList(res.data.data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    
+  };
+
+  const clearResults = () => setSummonerList([]);
+
+  const renderedSummoners = summonerList.map((summoner, i) => {
+    return <Summoner summoner={summoner} key={i} />
+  })
+
   return (
     <div className="nav-bar">
       <ul>
@@ -16,12 +43,17 @@ const NavBar = () => {
         <li>
             <NavLink to="/stats"> Statistiques </NavLink>
         </li>
-
-        <input type="button"  value="Connexion" className="right" onClick={() => {window.location.href = '/connexion';}}/>
-
-        {/* Redirect on url on click */}
         
-        <input type="text" placeholder="Search..." className="right"/>
+        <li className="right"> 
+            <input type="button" value="Connexion" />
+        </li>
+
+        <li className="right">
+          <SearchBar onSearchSubmit={onSearchSubmit} clearResults={clearResults}/>
+          <div className='renderedSummoners'>
+            {renderedSummoners}
+          </div>
+        </li>
 
       </ul>
     </div>
