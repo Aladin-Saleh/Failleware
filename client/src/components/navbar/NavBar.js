1cea8c5f095ed0d00879e746936e940520f127aa
Argail GISCLARD <GISCLARD_A@RIOC.LAB>
2023-04-19 13:18:48 +0200

    emty

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -12,7 +12,7 @@ const NavBar = () => {
     console.log(event.target.value);
     axios
     .get(`http://172.24.9.146:5000/api/fiware/summoner/${event.target.value}`)
-    .then(response => {setSummonerList(repsonse.data);})
+    .then(response => {setSummonerList(response.data);})
     .catch(error => console.log(error));
   }
 
